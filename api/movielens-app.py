4979b1d2cb92cee473936b26584bb66755ace2fb
Omar Rojas García <omarrojasg@gmail.com>
2020-05-17 18:51:47 -0500

    Ajustes varios

--- api/movielens-app.py
+++ api/movielens-app.py
@@ -5,7 +5,7 @@ import connexion
 app = Flask(__name__)
 
 USERNAME = "neo4j"
-PASS = "neo4j" #default
+PASS = "Grupo06" #default
 
 graph = Graph("bolt://localhost:7687", auth = (USERNAME, PASS))
 
@@ -14,29 +14,31 @@ graph = Graph("bolt://localhost:7687", auth = (USERNAME, PASS))
 # Get the available deatils of a given movie
 @app.route('/api/movie/details/<title>')
 def getMovieData(title):
-    matcher = NodeMatcher(graph)
-    movie = matcher.match("Movie", title={title}).first()
+    #matcher = NodeMatcher(graph)
+    #movie = matcher.match("Movie", title=title).first()
 
-    return jsonify(movie)
+    movies = graph.run('MATCH (m:Movie) WHERE m.title IN $title RETURN m', title = title)
+    
+    return jsonify(list(movies))
 
 # Get the genres associated with a given movie
 @app.route('/api/movie/genres/<title>')
 def getMovieGenres(title):
-    genres = graph.run('MATCH (genres)-[:IS_GENRE_OF]->(m:Movie {title: {title}}) RETURN genres', title=title)
+    genres = graph.run('MATCH (genres)-[:IS_GENRE_OF]->(m:Movie {title: $title}) RETURN genres', title=title)
 
     return jsonify(list(genres))
 
 # Get the submitted ratings of a given movie
 @app.route('/api/movie/ratings/<title>')
 def getMovieRatings(title):
-    ratings = graph.run('MATCH (u: User)-[r:RATED]->(m:Movie {title: {title}}) RETURN u.id AS user, r.rating AS rating', title=title)
+    ratings = graph.run('MATCH (u: User)-[r:RATED]->(m:Movie {title: $title}) RETURN u.id AS user, r.rating AS rating', title=title)
 
     return jsonify(ratings.data())
 
 # Get the submitted tags of a given movie
 @app.route('/api/movie/tags/<title>')
 def getMovieTags(title):
-    tags = graph.run('MATCH (u: User)-[t:TAGGED]->(m:Movie {title: {title}}) RETURN u.id AS user, t.tag AS tag', title=title)
+    tags = graph.run('MATCH (u: User)-[t:TAGGED]->(m:Movie {title: $title}) RETURN u.id AS user, t.tag AS tag', title=title)
 
     return jsonify(tags.data())
 
@@ -44,14 +46,14 @@ def getMovieTags(title):
 # Get list of movies from a given year
 @app.route('/api/movie/year/<year>')
 def getMoviesByYear(year):
-    movies = graph.run('MATCH (m:Movie) where m.year = {year} RETURN m.title AS title, m.year AS year', year=year)
+    movies = graph.run('MATCH (m:Movie) where m.year = $year RETURN m.title AS title, m.year AS year', year=year)
 
     return jsonify(movies.data())
 
 # Get the average rating for a given movie
 @app.route('/api/movie/average-rating/<title>')
 def getMovieAverageRating(title):
-    avg = graph.run('MATCH (u: User)-[r:RATED]->(m:Movie {title: {title}}) RETURN m.title AS title, avg(toFloat(r.rating)) AS averageRating', title=title)
+    avg = graph.run('MATCH (u: User)-[r:RATED]->(m:Movie {title: $title}) RETURN m.title AS title, avg(toFloat(r.rating)) AS averageRating', title=title)
 
     return jsonify(avg.data())
 
@@ -60,14 +62,14 @@ def getMovieAverageRating(title):
 # Get top N highest rated movies
 @app.route('/api/top/movie/top-n/<n>')
 def getMovieTopN(n):
-    mvs = graph.run('MATCH (u: User )-[r:RATED]->(m:Movie) RETURN m.title AS title, avg(r.rating) AS averageRating order by averageRating desc limit toInt({n})', n=n)
+    mvs = graph.run('MATCH (u: User )-[r:RATED]->(m:Movie) RETURN m.title AS title, avg(r.rating) AS averageRating order by averageRating desc limit toInt($n)', n=n)
 
     return mvs.data()
 
 # Get top N most rated movies
 @app.route('/api/top/movie/n-most-rated/<n>')
 def getMovieNMostRated(n):
-    mvs = graph.run('MATCH (u: User )-[r:RATED]->(m:Movie) RETURN m.title AS title, count(r.rating) as NumberOfRatings order by NumberOfRatings desc limit toInt({n})', n=n)
+    mvs = graph.run('MATCH (u: User )-[r:RATED]->(m:Movie) RETURN m.title AS title, count(r.rating) as NumberOfRatings order by NumberOfRatings desc limit toInt($n)', n=n)
 
     return mvs.data()
 
@@ -76,21 +78,21 @@ def getMovieNMostRated(n):
 # Get the submitted ratings by a given user
 @app.route('/api/user/ratings/<userId>')
 def getUserRatings(userId):
-    ratings = graph.run('MATCH (u:User {id: {userId}})-[r:RATED ]->(movies) RETURN movies.title AS movie, r.rating AS rating', userId=userId)
+    ratings = graph.run('MATCH (u:User {id: $userId})-[r:RATED ]->(movies) RETURN movies.title AS movie, r.rating AS rating', userId=userId)
 
     return jsonify(ratings.data())
 
 # Get the submitted tags by a given user
 @app.route('/api/user/tags/<userId>')
 def getUserTags(userId):
-    tags = graph.run('MATCH (u:User {id: {userId}})-[t:TAGGED ]->(movies) RETURN movies.title AS title, t.tag AS tag', userId=userId)
+    tags = graph.run('MATCH (u:User {id: $userId})-[t:TAGGED ]->(movies) RETURN movies.title AS title, t.tag AS tag', userId=userId)
 
     return jsonify(tags.data())
 
 # Get the average rating by a given user
 @app.route('/api/user/average-rating/<userId>')
 def getUserAverageRating(userId):
-    avg = graph.run('MATCH (u: User {id: {userId}})-[r:RATED]->(m:Movie) RETURN u.id AS user, avg(toFloat(r.rating)) AS averageRating', userId=userId)
+    avg = graph.run('MATCH (u: User {id: $userId})-[r:RATED]->(m:Movie) RETURN u.id AS user, avg(toFloat(r.rating)) AS averageRating', userId=userId)
 
     return jsonify(avg.data())
 
@@ -101,10 +103,10 @@ def getUserAverageRating(userId):
 
 def getRecContent(title,n):
     avg = graph.run('MATCH (m:Movie)<-[:IS_GENRE_OF]-(g:Genre)-[:IS_GENRE_OF]->(rec:Movie) '
-                    'WHERE m.title = {title} '
+                    'WHERE m.title = $title '
                     'WITH rec, COLLECT(g.name) AS genres, COUNT(*) AS numberOfSharedGenres '
                     'RETURN rec.title as title, genres, numberOfSharedGenres '
-                    'ORDER BY numberOfSharedGenres DESC LIMIT toInt({n});', title=title, n=n)
+                    'ORDER BY numberOfSharedGenres DESC LIMIT toInteger($n);', title=title, n=n)
 
     return jsonify(avg.data())
 
@@ -112,7 +114,7 @@ def getRecContent(title,n):
 @app.route('/api/rec_engine/collab/<userid>/<n>')
 
 def getRecCollab(userid,n):
-    rec = graph.run('MATCH (u1:User {id:{userid}})-[r:RATED]->(m:Movie) '
+    rec = graph.run('MATCH (u1:User {id:$userid})-[r:RATED]->(m:Movie) '
                     'WITH u1, avg(r.rating) AS u1_mean '
                     'MATCH (u1)-[r1:RATED]->(m:Movie)<-[r2:RATED]-(u2) '
                     'WITH u1, u1_mean, u2, COLLECT({r1: r1, r2: r2}) AS ratings WHERE size(ratings) > 10 '
@@ -125,7 +127,7 @@ def getRecCollab(userid,n):
                     'ORDER BY pearson DESC LIMIT 10 '
                     'MATCH (u2)-[r:RATED]->(m:Movie) WHERE NOT EXISTS( (u1)-[:RATED]->(m) ) '
                     'RETURN m.title AS title, SUM( pearson * r.rating) AS score '
-                    'ORDER BY score DESC LIMIT toInt({n});', userid=userid, n=n)
+                    'ORDER BY score DESC LIMIT toInteger($n);', userid=userid, n=n)
 
     return jsonify(rec.data())
 
---
+++ api/run.sh
@@ -0,0 +1,4 @@
+export FLASK_CONFIG=development
+export FLASK_APP=movielens-app.py
+flask run --host=0.0.0.0 --port=9000
+
